b2e4dc34757355fd9db3d65fbce93a5270b29b72
Weinan Qiu <davidiamyou@gmail.com>
2019-07-25 14:27:54 +0800

    bottom up merge sorts

--- src/main/java/io/imulab/review/sort/BottomUpMergeSort.java
+++ src/main/java/io/imulab/review/sort/BottomUpMergeSort.java
@@ -1,46 +1,39 @@
 package io.imulab.review.sort;
 
 /**
- * Merge sort works by dividing the target into a left portion and a right portion. Assuming both portions are also
- * sorted, they can be merged together to create a also sorted target. In order to achieve such assumption, the division
- * is done recursively until the left and right is only one element each.
+ * The bottom up merge sort views the {@link Sortable} of size N as N numbers of already sorted {@link Sortable} of
+ * size 1. Based on that, we will just merge every 2 elements, then every 4 elements, and so on... The sequence of
+ * 1, 2, 4, 8... reflects the maximum size of both sub targets that will be merged.
  *
- * Note the merge process requires extra space, we need to copy both portions out and then merge them back in to the
- * original target.
- *
- * Time complexity: O(NlgN), Merge process is O(N), Recursive process is O(lgN)
- * Space complexity: O(N), Actually requiring O(2N), but this is still O(N)
+ * Time complexity: O(NlgN), the stepping iteration is O(lgN), merge is still O(N)
+ * Space complexity: O(N), only needs space equal to an extra auxiliary array
  */
-public class MergeSort {
+public class BottomUpMergeSort {
 
     public static void sort(Sortable target) {
         sort(target, Sortable.Direction.ASC);
     }
 
     public static void sort(Sortable target, Sortable.Direction direction) {
-        System.out.println("Before: " + target.toString());
+        System.out.println("Before: " + target);
 
-        sort(target, 0, target.len() - 1, direction);
+        for (int N = 1; N < target.len(); N*=2) {
+            sort(target, N, direction);
+        }
 
-        System.out.println("After: " + target.toString());
+        System.out.println("After: " + target);
     }
 
-    private static void sort(Sortable target, int lowInclusive, int highInclusive, Sortable.Direction direction) {
-        if (highInclusive <= lowInclusive)
-            return;
-
-        int mid = (lowInclusive + highInclusive) / 2;
-
-        sort(target, lowInclusive, mid, direction);
-        sort(target, mid + 1, highInclusive, direction);
-
-        merge(target, lowInclusive, mid, highInclusive, direction);
+    private static void sort(Sortable target, int step, Sortable.Direction direction) {
+        for (int low = 0 ; low < target.len() - step; low += step * 2) {
+            int mid = low + step - 1;   // the mid index needs to reflect the size of step, always!
+            int high = Math.min(low + step + step - 1, target.len() - 1);   // the high index needs to be capped!
+            merge(target, low, mid, high, direction);
+        }
     }
 
-    @SuppressWarnings({"unchecked", "Duplicates"})
+    @SuppressWarnings({"Duplicates", "unchecked"})
     private static void merge(Sortable target, int lowInclusive, int mid, int highInclusive, Sortable.Direction direction) {
-        assert lowInclusive <= mid && mid <= highInclusive;
-
         Sortable first = target.subTarget(lowInclusive, mid + 1);
         Sortable second = target.subTarget(mid + 1, highInclusive + 1);
 
@@ -50,21 +43,16 @@ public class MergeSort {
         int i = 0, j = 0;
 
         for (int k = lowInclusive; k <= highInclusive; k++) {
-
-            // first is exhausted, unload the remaining of the second
             if (i >= first.len()) {
                 target.assign(second.get(j++), k);
                 continue;
             }
 
-            // second is exhausted, unload the remaining of the first
             if (j >= second.len()) {
                 target.assign(first.get(i++), k);
                 continue;
             }
 
-            // when ascending, add the lesser one first
-            // when descending, add the greater one first
             switch (direction) {
                 case ASC:
                     if (lessThan(first.get(i), second.get(j))) {
